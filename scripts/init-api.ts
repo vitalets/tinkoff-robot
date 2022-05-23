0eba8fe318c0d1515d6a426fe7945d623b5fcb01
Vitaliy Potapov <noginsk@rambler.ru>
2022-05-23 17:59:39 +0300

    backtest via local broker

--- scripts/init-api.ts
+++ scripts/init-api.ts
@@ -5,3 +5,9 @@ export const api = new TinkoffInvestApi({
   token: process.env.TINKOFF_API_TOKEN!,
   appName: 'vitalets/tinkoff-robot',
 });
+
+export const backtestApi = new TinkoffInvestApi({
+  endpoint: 'localhost:8080',
+  token: process.env.TINKOFF_API_TOKEN!,
+  appName: 'vitalets/tinkoff-robot',
+});
--- scripts/run-backtest.ts
+++ scripts/run-backtest.ts
@@ -2,27 +2,33 @@
  * Бэктест стратегии на исторических свечах:
  * npx ts-node-esm scripts/run-backtest.ts
  * DEBUG=tinkoff-invest-api:* npx ts-node-esm scripts/run-backtest.ts
+ *
+ * Предварительно нужно запустить сервер tinkoff-local-broker.
  */
-import { Backtest, Helpers } from 'tinkoff-invest-api';
+import MockDate from 'mockdate';
+import { Helpers } from 'tinkoff-invest-api';
 import { Robot } from '../src/robot.js';
 import { config } from '../src/config.js';
 import { OperationState, OperationType } from 'tinkoff-invest-api/dist/generated/operations.js';
+import { CandleInterval } from 'tinkoff-invest-api/dist/generated/marketdata.js';
+import { backtestApi as api } from './init-api.js';
 
+// Для бэктеста оставляем только первую стратегию
+config.strategies = config.strategies.slice(0, 1);
 const strategyConfig = config.strategies[0];
-
-const backtest = new Backtest({
-  token: process.env.TINKOFF_API_TOKEN!,
-  candleInterval: strategyConfig.interval,
-  ...Helpers.fromTo('1d', new Date('2022-05-18T10:00:00+03:00')),
-});
 
 main();
 
 async function main() {
-  console.warn(`Бэктест стратегии на исторических свечах...`);
-  const robot = new Robot(backtest.api, {...config, logLevel: 'info' });
+  await configureBroker({
+    from: new Date('2022-04-29T10:00:00+03:00'),
+    to: new Date('2022-04-29T19:00:00+03:00'),
+    candleInterval: CandleInterval.CANDLE_INTERVAL_1_MIN,
+  });
 
-  while (await backtest.tick()) {
+  const robot = new Robot(api, { ...config, logLevel: 'info' });
+
+  while (await tick()) {
     await robot.runOnce();
   }
 
@@ -31,13 +37,13 @@ async function main() {
 }
 
 async function showExpectedYield() {
-  const { expectedYield } = await backtest.api.operations.getPortfolio({ accountId: '' });
+  const { expectedYield } = await api.operations.getPortfolio({ accountId: '' });
   console.log(`Прибыль: ${Helpers.toNumber(expectedYield)}%`);
 }
 
 async function showOperations() {
   console.log(`Операции:`);
-  const { operations } = await backtest.operations.getOperations({
+  const { operations } = await api.operations.getOperations({
     figi: strategyConfig.figi,
     state: OperationState.OPERATION_STATE_EXECUTED,
     accountId: ''
@@ -51,8 +57,36 @@ async function showOperations() {
       o.type,
       o.figi,
       o.quantity > 0 && `(${o.quantity})`,
-      `${backtest.api.helpers.toNumber(o.payment)} ${o.payment?.currency}`,
+      `${api.helpers.toNumber(o.payment)} ${o.payment?.currency}`,
     ].filter(Boolean).join(' ');
     console.log(s);
   });
+}
+
+async function configureBroker(config: unknown) {
+  await api.orders.postOrder({
+    accountId: 'config',
+    figi: JSON.stringify(config),
+    quantity: 0,
+    direction: 0,
+    orderType: 0,
+    orderId: '',
+  });
+}
+
+async function tick() {
+  const res = await api.orders.postOrder({
+    accountId: 'tick',
+    figi: '',
+    quantity: 0,
+    direction: 0,
+    orderType: 0,
+    orderId: '',
+  });
+  if (res.message) {
+    MockDate.set(new Date(res.message));
+    return true;
+  } else {
+    return false;
+  }
 }
