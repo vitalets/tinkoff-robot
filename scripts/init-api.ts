6b400d67a5a0e1168558ea2fb09561b69b461cb5
Vitaliy Potapov <noginsk@rambler.ru>
2022-05-23 22:48:01 +0300

    check token

--- scripts/init-api.ts
+++ scripts/init-api.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 import { TinkoffInvestApi } from 'tinkoff-invest-api';
 
+const token = process.env.TINKOFF_API_TOKEN;
+const appName = 'vitalets/tinkoff-robot';
+
+if (!token) throw new Error(`Не указан токен.`);
+
 export const api = new TinkoffInvestApi({
-  token: process.env.TINKOFF_API_TOKEN!,
-  appName: 'vitalets/tinkoff-robot',
+  token,
+  appName,
 });
 
 export const backtestApi = new TinkoffInvestApi({
+  token,
+  appName,
   endpoint: 'localhost:8080',
-  token: process.env.TINKOFF_API_TOKEN!,
-  appName: 'vitalets/tinkoff-robot',
 });
